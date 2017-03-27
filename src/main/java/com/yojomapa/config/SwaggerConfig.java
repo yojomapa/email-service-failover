1940d36cb882c376a03f1d220cfe89d366f31a25
Jorge Palacio <jpalacio@wfscorp.com>
2017-03-27 09:11:35 -0500

    Add API Info Description

--- src/main/java/com/yojomapa/config/SwaggerConfig.java
+++ src/main/java/com/yojomapa/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.yojomapa.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,9 +19,17 @@ public class SwaggerConfig {
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.regex("/email.*"))
+            .build();
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder().title("Overview").description("Rest API for Email Service Failover")
+            .termsOfServiceUrl("http://springfox.io").license("Apache License Version 2.0")
+            .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE").version("2.0")
             .build();
   }
 }
